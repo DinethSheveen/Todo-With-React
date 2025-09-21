ca448bff3898e64c25a69e3cd828e29fc85beaf5
DinethSheveen <fdineth4000@gmail.com>
2025-09-21 14:18:58 +0100

    Basic structure of the application

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,19 @@ import React from 'react'
 
 function App() {
   return (
-    <div>App</div>
+    <div className='app-container'>
+      <div className="todo-container">
+        <div className="heading">
+          <h2>Task</h2>
+          <h2>Due Date</h2>
+          <h2>Controls</h2>
+        </div>
+
+        <div className="task-container">
+          
+        </div>
+      </div>
+    </div>
   )
 }
 
