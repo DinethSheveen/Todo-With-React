ad74bed48bce93a3181b44ec8d4bd1bf9f547e29
DinethSheveen <fdineth4000@gmail.com>
2025-09-21 17:41:36 +0100

    Added state functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,54 @@
-import React from 'react'
+import React, { useState } from 'react'
+import "./index.css"
 
 function App() {
+
+  const [tasks,setTasks] = useState([])
+  const [date,setDate] = useState("")
+  const [duty,setDuty] = useState("")
+
+  function handleDuty(event){
+    setDuty(event.target.value)
+  }
+
+  function handleDate(event){
+    setDate(event.target.value)
+  }
+
+  function addTask(){
+    const newTask = {
+                      task : duty,
+                      dueDate : date
+                    }
+
+    setTasks(prevTasks=>[...prevTasks,newTask])
+  }
+
   return (
     <div className='app-container'>
-      <div className="todo-container">
+      <h1 style={{textAlign:"center"}}>Todo App with React</h1>
+      <div className="controls">
+        <input type="text" value={duty} onChange={handleDuty} className='task-input' placeholder='Enter your todo here...'/>
+        <input type="date" value={date} onChange={handleDate} className='date-input' placeholder='Enter the due date here...'/>
+        <button onClick={addTask} className='add-btn'>Add Task</button>
+      </div>
+
+      <div className="task-showcase">
         <div className="heading">
-          <h2>Task</h2>
-          <h2>Due Date</h2>
-          <h2>Controls</h2>
+          <div><h2>Task</h2></div>
+          <div><h2>Date</h2></div>
+          <div><h2>Controls</h2></div>
         </div>
 
-        <div className="task-container">
-          
-        </div>
+        {tasks.map((task,index)=>{
+          return(
+            <div key={index} className='tasks'>
+              <div>{task.task}</div>
+              <div>{task.dueDate}</div>
+              <div>Delete</div>
+            </div>
+          ) 
+        })}
       </div>
     </div>
   )
